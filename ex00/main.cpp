5cbc835f4166bf5edcd2d6e2e05b969b5006cb44
Tudor-Ursescu <ursescu.tudor@gmail.com>
2025-04-07 14:46:04 +0200

    complete main for ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: turescu <turescu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 16:22:52 by turescu           #+#    #+#             */
-/*   Updated: 2025/04/06 17:10:34 by turescu          ###   ########.fr       */
+/*   Updated: 2025/04/07 14:10:02 by turescu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,19 @@ int main( void ) {
     ::swap( a, b );
 
     std::cout << BLUE << "AFTER SWITCH: " << std::endl;
-    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "a = " << a << ", b = " << b << RESET << std::endl;
 
-    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::cout << CYAN << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << RESET << std::endl;
     std::string c = "chaine1";
     std::string d = "chaine2";
+    std::cout << YELLOW << "ORIGINAL VALUES: " << std::endl;
+    std::cout << "c = " << c << ", d = " << d << RESET << std::endl;
     ::swap(c, d);
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    std::cout << BLUE << "AFTER SWITCH: " << std::endl;
+    std::cout << "c = " << c << ", d = " << d <<  RESET << std::endl;
+    std::cout << CYAN << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << RESET << std::endl;
+
     return 0;
     }
